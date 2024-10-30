b03cf85788a6eb9177cbd31a1e99ee6b3f04cc6d
Kelvin <94326858+Cyphermoon@users.noreply.github.com>
2024-10-30 14:59:11 +0000

    chore: installed zustand

--- src/app/(routes)/communication_method_selection/page.tsx
+++ src/app/(routes)/communication_method_selection/page.tsx
@@ -22,7 +22,7 @@ const CommunicationMethodSelection = () => {
       </h4>
 
       <div className='flex flex-col items-center lg:items-start space-y-8 lg:space-y-0 lg:flex-row justify-between w-full max-w-lg'>
-        <CommunicationMethod handleClick={handleMethodSelected} value='voice' icon={<GiSoundWaves className='text-5xl' />} text="Voice" />
+        <CommunicationMethod handleClick={handleMethodSelected} value='voice' icon={<GiSoundWaves className='text-4xl lg:text-5xl' />} text="Voice" />
         <CommunicationMethod handleClick={handleMethodSelected} value='keyboard' icon={<FaKeyboard className='text-3xl lg:text-4xl' />} text="Keyboard" />
       </div>
     </div>
